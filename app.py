9aab49bdfd3ce0ef08cc0baee6bc0efd5b83f5cd
42lince <42.duxiaoxi@gmail.com>
2016-11-29 01:13:20 +0000

    add some changes

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 from datetime import datetime
+import requests
+from subprocess import check_output
+import tempfile
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def time():
-	begin = datetime(1970, 1, 1, 0, 0, 0, 0)
-	delta = (datetime.utcnow() - begin).total_seconds()
-	return "UTC Time Now: " + str(round(delta))
+@app.route("/api/num_colors")
+def colors():
+    url = request.args.get('src')
+    r = requests.get(url)
+    temp = tempfile.NamedTemporaryFile()
+    with open(temp.name, 'wb') as fd:
+        fd.write(r.content)
+    result = check_output(["identify", "-format", "%k", temp.name])
+    return result
 
 if __name__ == "__main__":
     app.run()
